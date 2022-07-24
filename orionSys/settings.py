33355b45b684a2985b9271d1a78e4c3b7fad0327
Galikhan-bitloman <57861201+Galikhan-bitloman@users.noreply.github.com>
2022-07-25 01:10:20 +0600

    Update settings.py

--- orionSys/settings.py
+++ orionSys/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import datetime
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rg7da9etswjpe*9ue2hcmvb+98j+wn(=$a#(yqh#f%c)&9cn3b'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
